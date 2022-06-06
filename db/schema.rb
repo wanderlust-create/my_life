6c860aff6a372739f2286236c1400b00c9e42729
Tamara Dowis <tamara.dowis@gmail.com>
2022-06-05 19:22:17 -0500

    create user_affirmations model

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_605_231_449) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_001518) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'elephants', force: :cascade do |t|
-    t.string 'life'
-    t.text 'description'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_elephants_on_user_id'
+  create_table "elephants", force: :cascade do |t|
+    t.string "life"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_elephants_on_user_id"
   end
 
-  create_table 'pieces', force: :cascade do |t|
-    t.integer 'points'
-    t.text 'description'
-    t.integer 'time_minutes'
-    t.bigint 'elephant_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['elephant_id'], name: 'index_pieces_on_elephant_id'
+  create_table "pieces", force: :cascade do |t|
+    t.integer "points"
+    t.text "description"
+    t.integer "time_minutes"
+    t.bigint "elephant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["elephant_id"], name: "index_pieces_on_elephant_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'user_name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "user_affirmations", force: :cascade do |t|
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_affirmations_on_user_id"
   end
 
-  add_foreign_key 'elephants', 'users'
-  add_foreign_key 'pieces', 'elephants'
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "elephants", "users"
+  add_foreign_key "pieces", "elephants"
+  add_foreign_key "user_affirmations", "users"
 end
---
+++ spec/factories/user_affirmations.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :user_affirmation do
+    description { "MyText" }
+  end
+end
