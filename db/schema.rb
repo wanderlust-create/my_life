360fd57acbe3c5d3f17885b99bc2d87dfad2be5b
Tamara Dowis <tamara.dowis@gmail.com>
2022-06-07 16:16:44 -0500

    add recurring attribute to Pieces

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_606_001_518) do
+ActiveRecord::Schema[7.0].define(version: 20_220_607_211_542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 20_220_606_001_518) do
     t.bigint 'elephant_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'recurring'
     t.index ['elephant_id'], name: 'index_pieces_on_elephant_id'
   end
 
