1e4b256c06dea7f95d5cf92c7149bf9514b51d5d
Tamara Dowis <tamara.dowis@gmail.com>
2022-06-07 13:31:33 -0500

    Affirmations service request with caching

---
+++ app/services/affirmations_service.rb
@@ -0,0 +1,10 @@
+class AffirmationsService
+
+  def self.get_affirmations(category)
+    Rails.cache.fetch("get_affirmations_#{category}", expires_in: 24.hours) do
+      url = "https://mylifeaffirmations.herokuapp.com/api/v1/affirmations?category=#{category}"
+      response = Faraday.get(url)
+      data = JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
